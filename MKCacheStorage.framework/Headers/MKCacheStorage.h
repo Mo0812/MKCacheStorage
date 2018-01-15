fe05b6dba3e5a41011e456f650aca9b6c0edf130
mkanzler <developer@moritzkanzler.de>
2018-01-15 15:24:15 +0100

    added universal framework

---
+++ MKCacheStorage.framework/Headers/MKCacheStorage.h
@@ -0,0 +1,19 @@
+//
+//  MKCacheStorage.h
+//  MKCacheStorage
+//
+//  Created by Moritz Kanzler on 02.01.18.
+//  Copyright © 2018 Moritz Kanzler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKCacheStorage.
+FOUNDATION_EXPORT double MKCacheStorageVersionNumber;
+
+//! Project version string for MKCacheStorage.
+FOUNDATION_EXPORT const unsigned char MKCacheStorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKCacheStorage/PublicHeader.h>
+
+
