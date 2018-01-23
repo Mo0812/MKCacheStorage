8baf0e770bbc324258cc45af24ded3fdf5729833
mkanzler <developer@moritzkanzler.de>
2018-01-23 17:09:32 +0100

    finalizing v 1.0

--- MKCacheStorage/MKCacheStorage.swift
+++ MKCacheStorage/MKCacheStorage.swift
@@ -10,7 +10,7 @@ import Foundation
 
 open class MKCacheStorage {
     
-    static let shared = MKCacheStorage(debugInfo: false)
+    open static let shared = MKCacheStorage(debugInfo: false)
     
     let storageHandler: MKCSStorageHandler?
     let indexHandler: MKCSSecondaryIndices?
---
+++ build/MKCacheStorage.build/Debug-iphoneos/MKCacheStorage.build/Objects-normal/arm64/MKCacheStorage_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/mkanzler/Dropbox/Projekte/Swift/MKCacheStorage/build/MKCacheStorage.build/Debug-iphoneos/MKCacheStorage.build/DerivedSources/MKCacheStorage_vers.c
